db85f2181c56a781144bc9b95f246f9cb2ab5b07
ShraddhaK26 <shraddhakupekar@gmail.com>
2023-08-05 23:07:33 +0530

    program

---
+++ datatype.js
@@ -0,0 +1,58 @@
+//There are 8 types 
+//basically divided into two parts premitive and nonpremitive
+
+//1. string  :
+var address ="A/P-Hiralage Tal-Gadhinglaj Dist-Kolhapur"
+console.log(address);
+
+//2. Number
+let amount = 100000;
+console.log(amount);
+let amt = 105.232;
+console.log(amt);
+amount = 50000;
+console.log(amount);
+
+//3. boolean
+let abc=true
+console.log(abc);
+console.log(typeof(abc));
+
+//4. undefined
+let sk;
+console.log(sk);
+
+//5. null data: data type of null : object
+let monkey=null;
+console.log(monkey);
+
+//6. object:
+let emppdata={
+    emppname:  'shraddha',
+    emppage :21,
+    emppadd : 'kop'
+}
+
+console.log(emppdata);
+console.log(typeof(emppdata));
+
+//7. empty array
+//let list = [];
+let list = ['i10','volvo','swift','audi'];
+console.log(list);
+console.log(typeof(list));
+
+let h = 10.0;
+let l = h.toString();
+console.log('boolean value',l);
+console.log('boolean value',typeof(l));
+
+let savingAmount = BigInt('9999999999999999');
+let amount2= 9999999999999999n;
+console.log(amount2);
+console.log(savingAmount);
+
+let r = '99999999999999999';
+console.log(r);
+
+
